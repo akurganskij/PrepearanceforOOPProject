7bfb7c5d6ce53b9a30b64a73fe0d615380f00152
Андрей Курганский <kurganskij@live.ru>
2021-11-16 00:00:33 +0300

    Андрей 16.11 добавил вращ. прямоугольник

--- WinFormsApp20/Form1.cs
+++ WinFormsApp20/Form1.cs
@@ -12,10 +12,48 @@ namespace WinFormsApp20
 {
     public partial class Form1 : Form
     {
+        Rectangle rect = new Rectangle(300, 100, 200, 300);
+        SolidBrush br = new SolidBrush(Color.Red);
+        double angle = 0;
         public Form1()
         {
             InitializeComponent();
         }
 
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {            
+            PaintEventArgs paint = new PaintEventArgs(CreateGraphics(), rect);
+            Rect_Paint(sender, paint);        
+        }
+        private void Rect_Paint(object sender, PaintEventArgs e)
+        {
+            
+            e.Graphics.RotateTransform((float)angle, System.Drawing.Drawing2D.MatrixOrder.Append);
+            e.Graphics.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.HighQuality;
+            e.Graphics.FillRectangle(br, rect);
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            angle += 1;
+            this.Text = angle.ToString();
+            this.Refresh();
+        }
+
+        private void Form1_Paint1(object sender, PaintEventArgs e)
+        {
+            e.Graphics.RotateTransform((float)angle);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+            br.Color = Color.FromArgb(rnd.Next(0, 256), rnd.Next(0, 256), rnd.Next(0, 256));
+            this.Refresh();
+        }
     }
 }
